c67d3dc02bf1f43a53cca9cc8c9767b33a1189b5
Alberto C <80642984+btocarmona2021@users.noreply.github.com>
2023-06-11 16:28:18 -0300

    Etapa 3

--- Guia_12_Extra_02/src/vista/Test.java
+++ Guia_12_Extra_02/src/vista/Test.java
@@ -14,6 +14,7 @@ public class Test {
         edificios.add(new Polideportivo("Polideportivo Sur", false, 100, 6, 40));
         edificios.add(new EdificioDeOficina(12, 30, 12, 50, 36, 50));
         edificios.add(new EdificioDeOficina(6, 20, 10, 40, 30, 20));
+        
         int techados = 0;
         for (int i = 0; i < edificios.size(); i++) {
 
@@ -22,7 +23,9 @@ public class Test {
             System.out.print("El volumen del edificio " + (i + 1) + " es ");
             System.out.println(edificios.get(i).calcularVolumen() + " Mt³");
 
+            
             if (edificios.get(i) instanceof Polideportivo) {
+                
                 Polideportivo poli = (Polideportivo) edificios.get(i);
                 if (poli.isTipoInstalacion() == true) {
                     techados++;
