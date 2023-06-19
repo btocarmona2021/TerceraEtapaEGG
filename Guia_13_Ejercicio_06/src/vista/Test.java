82f924436ca60675d9f051e2a443fee4d0765a21
Alberto C <80642984+btocarmona2021@users.noreply.github.com>
2023-06-19 20:50:46 -0300

    Update Test.java

---
+++ Guia_13_Ejercicio_06/src/vista/Test.java
@@ -0,0 +1,26 @@
+package vista;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        /*
+        Si se produce la excepcion se ejecutarian la sentencias siguientes
+        sentencia_a1
+        sentencia_a2
+        sentencia_a3 -> donde se produce el error y ejecutaria MioExcepcion ejecutando
+        la sentencia
+        sentencia_a6
+         */
+        
+        /*
+        Si no se produce la excepcion las siguientes extenciones se ejecutarian
+        
+        sentencia_a1
+        sentencia_a2
+        sentencia_a3
+        sentencia_a4
+        
+        */
+    }
+}
