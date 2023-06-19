a2114f3a380d0b3862fbf562e11f393f4cd2033b
Alberto C <80642984+btocarmona2021@users.noreply.github.com>
2023-06-19 20:57:11 -0300

    Update Test.java

---
+++ Guia_13_Ejercicio_07/src/vista/Test.java
@@ -0,0 +1,23 @@
+package vista;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        /*
+        Si se produce la excepcion se ejecutarian las siguientes sentencias:
+        sentencia_b1
+        sentencia_b2 la que provoca la excepcion.
+        sentencia_b3 la excepcion.
+        sentencia_b4 el bloque finally que se ejecuta siempre.
+        */
+        
+          /*
+        Si no se produce la excepcion se ejecutarian las siguientes sentencias:
+        sentencia_b1
+        sentencia_b2
+        sentencia_b4 el bloque finally que se ejecuta siempre.
+        */
+        
+    }
+}
